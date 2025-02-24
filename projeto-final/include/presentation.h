ed605d79975e4ddd36a1ce3515a9502b4858ab15
MatheusNevs <matheusnf62@gmail.com>
2025-02-23 21:49:22 -0300

    update: presentation fixed and prettier

--- projeto-final/include/presentation.h
+++ projeto-final/include/presentation.h
@@ -3,13 +3,14 @@
 
 #include "ssd1306.h"
 #include <stdio.h>
+#include <string.h>
 
 void draw_daily_forecast(uint8_t *ssd, struct render_area *frame_area,
                          char *day, char *month, char *year,
                          char *minTemperature, char *maxTemperature);
 
 void draw_hourly_forecast(uint8_t *ssd, struct render_area *frame_area,
-                          char *day, char *month, char *year,
+                          char *hour, char *minute,
                           char *precipitation, char *temperature, char *humidity);
 
 #endif // PRESENTATION_INTERFACE_H
--- projeto-final/source/presentation.c
+++ projeto-final/source/presentation.c
@@ -4,14 +4,23 @@ void draw_daily_forecast(uint8_t *ssd, struct render_area *frame_area,
                          char *day, char *month, char *year,
                          char *minTemperature, char *maxTemperature)
 {
+  memset(ssd, 0, ssd1306_buffer_length);
+  printf("draw_daily_forecast");
+
   int y = 0;
 
   char formatted_date[32];
-  sprintf(formatted_date, "%s %s", month, day);
+  const char *months[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+  int month_num = atoi(month) - 1; // Convert month string to number (0-11)
+  sprintf(formatted_date, "%s %s, %s", months[month_num], day, year);
+
   char *text1[] = {
+      "Daily Forecast",
       formatted_date,
-      "      2025      ",
   };
+
+  printf(formatted_date, "\n");
+  printf(year, "\n");
 
   // Draw first text block
   for (uint i = 0; i < count_of(text1); i++)
@@ -36,6 +45,9 @@ void draw_daily_forecast(uint8_t *ssd, struct render_area *frame_area,
       min_temp,
   };
 
+  printf(max_temp, "\n");
+  printf(min_temp, "\n");
+
   for (uint i = 0; i < count_of(text2); i++)
   {
     ssd1306_draw_string(ssd, 0, y, text2[i]);
@@ -45,17 +57,21 @@ void draw_daily_forecast(uint8_t *ssd, struct render_area *frame_area,
 }
 
 void draw_hourly_forecast(uint8_t *ssd, struct render_area *frame_area,
-                          char *day, char *month, char *year,
+                          char *hour, char *minute,
                           char *precipitation, char *temperature, char *humidity)
 {
+  printf("draw_hourly_forecast");
+  memset(ssd, 0, ssd1306_buffer_length);
   int y = 0;
 
   char formatted_date[32];
-  sprintf(formatted_date, "%s %s", month, day);
+  sprintf(formatted_date, "    %sh %sm", hour, minute);
   char *text1[] = {
+      "Hourly Forecast",
       formatted_date,
-      "      2025      ",
   };
+
+  printf(formatted_date, "\n");
 
   // Draw first text block
   for (uint i = 0; i < count_of(text1); i++)
@@ -80,6 +96,10 @@ void draw_hourly_forecast(uint8_t *ssd, struct render_area *frame_area,
       temp,
       precip,
       hum};
+
+  printf(temp, "\n");
+  printf(precip, "\n");
+  printf(hum, "\n");
 
   for (uint i = 0; i < count_of(text2); i++)
   {
